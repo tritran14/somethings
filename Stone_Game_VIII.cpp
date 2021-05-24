9677252b3be9d64a7201c202260392f4b5b5766d
tritran14 <65666661+tritran14@users.noreply.github.com>
2021-05-24 14:33:08 +0700

    Update Stone_Game_VIII.cpp

--- Stone_Game_VIII.cpp
+++ Stone_Game_VIII.cpp
@@ -14,3 +14,31 @@ public:
         return dp[1];
     }
 };
+
+const ll INF=1e18;
+class Solution2 {
+public:
+    ll dp[100005][2];
+    
+    ll dfs(const vector<int>& a,int id,int turn,ll sum){
+        if(dp[id][turn]>-INF) return dp[id][turn];
+        ll& ans=dp[id][turn];
+        ans=-INF;
+        if(id==0){
+            sum+=a[id];
+            ++id;
+        }
+        if(id+1==(int)a.size()){
+            return a[id]+sum;
+        }
+        sum+=a[id];
+        ans=max(sum-dfs(a,id+1,turn^1,sum),dfs(a,id+1,turn,sum));
+        return ans;
+    }
+    
+    int stoneGameVIII(vector<int>& a) {
+        int n=(int)a.size();
+        for(int i=0;i<n;++i) dp[i][0]=dp[i][1]=-INF;
+        return dfs(a,0,0,0);
+    }
+};
